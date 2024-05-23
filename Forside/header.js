5e868a064017bac47c248bc5f050a71a8cf5eba4
kris124f <kripet02@EASV365.dk>
2024-05-23 14:55:29 +0200

    Tilføjet slider test
    ja det har jeg

---
+++ Forside/header.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger"); //div
+const navMenu = document.querySelector(".navbar__menu"); //ul with all menu links
+const navLink = document.querySelectorAll(".menuItem__link"); //a: all links in menu
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+  //open and close menu in mobile
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  //when a menu item (a) is clicked, the mobile menu is deactivated
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
