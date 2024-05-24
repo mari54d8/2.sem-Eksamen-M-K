9bb89fc52211e7c04a1113456adac60c3f79a82d
mari54d8 <marsim01@easv365.dk>
2024-05-24 10:33:01 +0200

    header

---
+++ imageslider.js
@@ -0,0 +1,22 @@
+const imgContainer = document.querySelector(".img__container")
+const btnLeft = document.querySelector(".btn-left")
+const btnRight = document.querySelector(".btn-right")
+const images = document.querySelectorAll(".img__container img")
+
+let currentSlide = 0
+
+function changeImage() {
+  imgContainer.style.transform = `translateX(-${
+    currentSlide = imgContainer.offsetWidth
+  }px)`
+}
+
+btnRight.addEventListener('click', function () {
+  currentSlide++
+  changeImage()
+})
+
+btnLeft.addEventListener('click', function () {
+  currentSlide--
+  changeImage()
+})
